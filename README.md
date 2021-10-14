a9de886a3846cfab118b8bccbe17674f6c940585
hank0316 <b08902126@ntu.edu.tw>
2021-10-14 08:48:47 +0800

    some modify for install package from our repo, please check README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # HEAR-2021-NeurIPS-Challenge---NTU
+
+## Installation of the package
+
+**This is a test for pip installable package of HuBERT_xlarge from our repo.**
+
+```
+$ ~/bin/python3.8/bin/python3.8 -m pip install git+https://github.com/tony10101105/HEAR-2021-NeurIPS-Challenge---NTU.git
+```
+
+## Usage
+
+```python3
+# In python code:
+from GARU import hubert_xlarge
+```
+
+## Note
+
+* Modififying the hubert_xlarge code should be in the `GARU` folder.
+* If we want to add new code into the package & make it importable, just put the file under the `GARU` folder.
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="toyzdog_test",
+    version="0.0.1",
+    author="toyz's dog",
+    author_email="b08902126@csie.ntu.edu.tw",
+    description="HEAR 2021",
+    long_description=long_description,
+    url="https://github.com/tony10101105/HEAR-2021-NeurIPS-Challenge---NTU.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
