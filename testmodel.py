197d763bb1b6c2057748e246196a1460f908d4cc
lcagma <puredzom1234@gmail.com>
2021-10-15 17:30:58 +0800

    delete unused file

--- testmodel.py
+++
@@ -1,9 +0,0 @@
-import torch
-from hubert_ft import load_model
-
-a = load_model()
-torch.save(a.state_dict(), 'toyzdog.pt')
-'''
-b = load_model(model_hub="facebook/wav2vec2-large-xlsr-53")
-torch.save(b.state_dict(), '/pretrained/weedtoyz.pt')
-'''
