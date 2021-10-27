49871c2815e08058a43a8f746fa1bd318c8a1bfe
hank0316 <b08902126@ntu.edu.tw>
2021-10-27 21:21:33 +0800

    make it can merge any number of embeddings

--- merge.py
+++ merge.py
@@ -6,6 +6,6 @@ def merge(*embeddings) -> torch.Tensor:
     All embeddings should be the same shape.
     """
     # Interleave embeddings.
-    merge_embeddings = torch.stack(embeddings, dim=3)
+    merge_embeddings = torch.stack(embeddings, dim=embeddings[0].dim())
     # Reshape: [108, 49, 1024, 3] -> [108, 49, 3072], then return the Tensor.
-    return torch.flatten(merge_embeddings, start_dim=2)
+    return torch.flatten(merge_embeddings, start_dim=(embeddings[0].dim-1))
