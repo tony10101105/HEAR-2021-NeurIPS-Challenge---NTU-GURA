c3647248245119f2217c8ddce640034c78e530c1
hsutim047 <b08201047@meow2.csie.ntu.edu.tw>
2021-11-16 15:53:30 +0800

    install_requires

--- setup.py
+++ setup.py
@@ -13,7 +13,12 @@ setuptools.setup(
     url="https://github.com/tony10101105/HEAR-2021-NeurIPS-Challenge---NTU.git",
     packages=setuptools.find_packages(),
     install_requires=[
-        "transformers==4.11.3",
+		"transformers==4.11.3",
+        "Torchcrepe==0.0.15",
+        "torch==1.9.1",
+        "torch>=1.10.0+cu111",
+        "tensorflow-gpu==2.7.0",
+        "abydos==0.5.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
