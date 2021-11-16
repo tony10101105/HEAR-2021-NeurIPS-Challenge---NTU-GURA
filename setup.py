313476c9a3cca63d08129e78fff8d33dafddaba8
hank0316 <b08902126@ntu.edu.tw>
2021-11-16 15:36:59 +0800

    >= -> ==

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     install_requires=[
         "transformers==4.11.3",
         "Torchcrepe==0.0.15",
-        "torch >= 1.9.1",
+        "torch==1.9.1",
         "tensorflow-gpu==2.7.0",
         "abydos==0.5.0",
     ],
