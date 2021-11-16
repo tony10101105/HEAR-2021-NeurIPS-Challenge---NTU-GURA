0a0c2d6b8bac94a4c2d19271c5d407e5886661a2
hank0316 <b08902126@ntu.edu.tw>
2021-11-16 13:49:49 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     install_requires=[
         "transformers==4.11.0",
         "Torchcrepe==0.0.15",
-        "torch==1.9.1+cu111",
+        "torch==1.9.1",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
